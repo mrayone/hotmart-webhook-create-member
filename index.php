b1d9c5d01bdb786ff9c25632cad277a0a930dc8b
Maycon Rayone Rodrigues Xavier <maycon.rayone@gmail.com>
2018-11-28 11:27:46 -0200

    * Iniciando o desenvolvimento.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * @package Hotmart_Webhook_User_Creat.
+ * @version 1.0
+ */
+/*
+Plugin Name:  Hotmart Webhook User Creat.
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Este plugins foi criado para fazer um usuário em seu blog quando houver uma confirmação de pagamento.
+Version:      1.0
+Author:       Maycon Rayone Rodrigues Xavier
+Author URI:   https://github.com/mrayone
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  hotwebhookuser
+Domain Path:  /languages
+*/
