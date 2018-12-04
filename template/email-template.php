b9afaf20b5c007fda5e451d2c5e37a3c605c122c
Maycon Rayone Rodrigues Xavier <maycon.rayone@gmail.com>
2018-12-04 12:46:21 -0200

    * Adicionando tradução.

--- template/email-template.php
+++ template/email-template.php
@@ -1,10 +1,17 @@
-<h2>Olá NOME_CLIENTE, aqui é o NOME_AUTOR </h2>
-Estou te enviando esse e-mail para parabenizá-lo por sua inscrição no curso CURSO_NOME e também para te passar os
-dados de acesso!
+<h2>
+    <?php __("Olá", 'hotwebhookuser'); ?> NOME_CLIENTE,
+    <?php echo __("aqui é o", ''); ?> NOME_AUTOR </h2>
+<?php echo __('Estou te enviando esse e-mail para parabenizá-lo por sua inscrição no curso CURSO_NOME e também para te passar os
+dados de acesso!','hotwebhookuser'); ?>
 
-<strong>Segue os dados:</strong>
+<strong>
+    <?php  echo __("Segue os dados:", 'hotwebhookuser'); ?></strong>
 <ul>
-    <li>Site para acesso:</li>
-    <li>Login: USU_LOGIN</li>
-    <li>Senha Temporária: <strong>USU_PASSWORD</strong></li>
+    <li>
+        <?php  echo __("Site para acesso:", 'hotwebhookuser'); ?>
+    </li>
+    <li>
+        <?php  echo __("Login:", 'hotwebhookuser'); ?> USU_LOGIN</li>
+    <li>
+        <?php  echo __("Senha Temporária:", 'hotwebhookuser'); ?> <strong>USU_PASSWORD</strong></li>
 </ul>
